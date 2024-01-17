544e4e4ebaf70dbe5d1233108bf7f0d04a137c32
chengpeng <chengpeng@blockchain.com>
2024-01-17 18:07:58 +0800

    feat: update

--- components/newsletter.tsx
+++ components/newsletter.tsx
@@ -3,15 +3,26 @@ export default function Newsletter() {
     <section>
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
         <div className="pb-12 md:pb-20">
-
           {/* CTA box */}
-          <div className="relative bg-gray-900 rounded py-10 px-8 md:py-16 md:px-12 shadow-2xl overflow-hidden" data-aos="zoom-y-out">
-
+          <div
+            className="relative bg-gray-900 rounded py-10 px-8 md:py-16 md:px-12 shadow-2xl overflow-hidden"
+            data-aos="zoom-y-out"
+          >
             {/* Background illustration */}
-            <div className="absolute right-0 bottom-0 pointer-events-none hidden lg:block" aria-hidden="true">
+            <div
+              className="absolute right-0 bottom-0 pointer-events-none hidden lg:block"
+              aria-hidden="true"
+            >
               <svg width="428" height="328" xmlns="http://www.w3.org/2000/svg">
                 <defs>
-                  <radialGradient cx="35.542%" cy="34.553%" fx="35.542%" fy="34.553%" r="96.031%" id="ni-a">
+                  <radialGradient
+                    cx="35.542%"
+                    cy="34.553%"
+                    fx="35.542%"
+                    fy="34.553%"
+                    r="96.031%"
+                    id="ni-a"
+                  >
                     <stop stopColor="#DFDFDF" offset="0%" />
                     <stop stopColor="#4C4C4C" offset="44.317%" />
                     <stop stopColor="#333" offset="100%" />
@@ -34,30 +45,66 @@ export default function Newsletter() {
             </div>
 
             <div className="relative flex flex-col lg:flex-row justify-between items-center">
-
               {/* CTA content */}
               <div className="text-center lg:text-left lg:max-w-xl">
-                <h3 className="h3 text-white mb-2">Want more tutorials & guides?</h3>
-                <p className="text-gray-300 text-lg mb-6">Lorem ipsum dolor sit amet consectetur adipisicing elit nemo expedita voluptas culpa sapiente.</p>
+                <h3 className="h3 text-white mb-2">お問い合わせ</h3>
+                <p className="text-gray-300 text-lg mb-6">
+                  ホームページ作成に関するご質問・ご相談・お問い合わせは、メールにて承っております
+                </p>
 
                 {/* CTA form */}
                 <form className="w-full lg:w-auto">
+                  <div>
+                    <label
+                      htmlFor="subject"
+                      className="block text-sm font-medium leading-6 text-white"
+                    >
+                      件名
+                    </label>
+                    <div className="mt-2">
+                      <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                        <input
+                          type="text"
+                          name="subject"
+                          id="subject"
+                          autoComplete="subject"
+                          className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-white placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                        />
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="col-span-full mb-6">
+                    <label
+                      htmlFor="content"
+                      className="block text-sm font-medium leading-6 text-gray-900"
+                    >
+                      お問い合わせ内容
+                    </label>
+                    <div className="mt-2">
+                      <textarea
+                        id="content"
+                        name="content"
+                        rows={3}
+                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        defaultValue={""}
+                      />
+                    </div>
+                  </div>
                   <div className="flex flex-col sm:flex-row justify-center max-w-xs mx-auto sm:max-w-md lg:mx-0">
-                    <input type="email" className="form-input w-full appearance-none bg-gray-800 border border-gray-700 focus:border-gray-600 rounded-sm px-4 py-3 mb-2 sm:mb-0 sm:mr-2 text-white placeholder-gray-500" placeholder="Your email…" aria-label="Your email…" />
-                    <a className="btn text-white bg-blue-600 hover:bg-blue-700 shadow" href="#0">Subscribe</a>
+                    <a className="btn text-white bg-blue-600 hover:bg-blue-700 shadow" href="#0">
+                      メールを送る
+                    </a>
                   </div>
                   {/* Success message */}
                   {/* <p className="text-sm text-gray-400 mt-3">Thanks for subscribing!</p> */}
-                  <p className="text-sm text-gray-400 mt-3">No spam. You can unsubscribe at any time.</p>
+                  <p className="text-sm text-gray-400 mt-3">お気軽にお問い合わせてください</p>
                 </form>
               </div>
-
             </div>
-
           </div>
-
         </div>
       </div>
     </section>
-  )
+  );
 }
